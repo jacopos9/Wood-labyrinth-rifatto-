b0e312fb39bc92bbd28d362e2021c0f23701b169
jacoposcuola <jacopo.scuola0@gmail.com>
2021-02-03 14:42:51 +0100

    ----

---
+++ wood labyrinth/Assets/All-GridProject 1/Scripts/TileBase.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace JsFramework
+{
+   /// <summary>
+   /// contiene tutta la logica di base per tutti i giochi quindi, solo la parte visiva
+   /// </summary>
+    public class TileBase : MonoBehaviour
+    {
+        public int x;
+        public int y;
+        public int id;
+    }
+
+}
+
+
